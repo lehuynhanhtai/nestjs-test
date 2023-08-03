b699c36a51e7177813c301b30a7973d1c3280e28
lehuynhanhtai <lehuynhanhtai@gmail.com>
2023-08-03 09:20:11 +0700

    25-Testing controller

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,20 +1,26 @@
 import { CreateMessageDto } from './dtos/create-message.dto';
 import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
 export class MessagesController {
+  messagesService: MessagesService;
+  constructor() {
+    this.messagesService = new MessagesService();
+  }
+
   @Get()
-  listMessages(): string {
-    return 'sasd';
+  listMessages() {
+    return this.messagesService.findAll();
   }
 
   @Post()
   createMessages(@Body() body: CreateMessageDto) {
-    console.log(body);
+    return this.messagesService.create(body.content);
   }
 
   @Get('/:id')
   getMessages(@Param('id') id: string) {
-    console.log(id);
+    return this.messagesService.findOne(id);
   }
 }
