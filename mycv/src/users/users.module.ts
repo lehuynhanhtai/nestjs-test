2fde75d3c4c124b52298fda706fe750c887501ef
lehuynhanhtai <lehuynhanhtai@gmail.com>
2023-08-04 15:39:16 +0700

    video 74: create new user

--- mycv/src/users/users.module.ts
+++ mycv/src/users/users.module.ts
@@ -3,10 +3,11 @@ import { Module } from '@nestjs/common';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 import { User } from './user.entity';
+import { AuthService } from './auth.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService],
-  imports: [TypeOrmModule.forFeature([User])]
+  providers: [UsersService, AuthService],
+  imports: [TypeOrmModule.forFeature([User])],
 })
 export class UsersModule {}
