4a9f3794c254e753f34272d1d8035ee90237dc24
lehuynhanhtai <lehuynhanhtai@gmail.com>
2023-08-06 15:47:35 +0700

    video 122

--- mycv/test/auth.e2e-spec.ts
+++ mycv/test/auth.e2e-spec.ts
@@ -16,7 +16,7 @@ describe('Authentication System', () => {
   });
 
   it('handles a signup request', () => {
-    const email = 'asdfasdasd1@gmail.com';
+    const email = 'asdfasd2asd1234123@gmail.com';
     return request(app.getHttpServer())
       .post('/auth/signup')
       .send({ email, password: 'asdfasd' })
@@ -26,5 +26,22 @@ describe('Authentication System', () => {
         expect(id).toBeDefined();
         expect(email).toEqual(email);
       });
+  });
+
+  it('signup as a user then get the currently logged in user', async () => {
+    const email = 'asdf123asdw@asdf.com';
+
+    const res = await request(app.getHttpServer())
+      .post('/auth/signup')
+      .send({ email, password: 'asdf' })
+      .expect(201);
+
+    const cookie = res.get('Set-Cookie');
+    const { body } = await request(app.getHttpServer())
+      .get('/auth/whoami')
+      .set('Cookie', cookie)
+      .expect(200);
+
+    expect(body.email).toEqual(email);
   });
 });
